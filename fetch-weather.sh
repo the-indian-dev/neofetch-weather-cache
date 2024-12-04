c46daad6f19e496274e7f97232c51e49d93df0c4
Ritabrata Das <ritabrata.das2669@gmail.com>
2024-12-04 13:59:19 +0530

    Bug Fix : Cache not getting updated in sleep

--- fetch-weather.sh
+++ fetch-weather.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if ! ping -c 1 -q google.com &>/dev/null; then
+    echo "No internet connection. Weather data not updated."
+    exit 1
+fi
+
+echo "Internet Connection Present"
+
 # Fetch weather data and save it to a cache file
 curl -s 'wttr.in/Bhubaneswar?format=3' > /home/ritabrata/.weather_cache
 
